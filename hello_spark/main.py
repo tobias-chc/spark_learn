401a0492a82269575dc81aca5b4e9c750ff4a854
Tobias Chavarria <tobias.chavarria.castro@gmail.com>
2023-06-17 13:43:46 +0200

    feat: add main script

---
+++ hello_spark/main.py
@@ -0,0 +1,25 @@
+import sys
+from pyspark.sql import SparkSession
+from lib.logger import Log4j
+from lib.utils import get_spark_app_conf
+
+SPARK_HOME="/Users/tobiaschavarria/Library/Caches/pypoetry/virtualenvs/spark-learn-iD0jGMx_-py3.11/lib/python3.11/site-packages/pyspark"
+
+if __name__ == "__main__":
+    conf = get_spark_app_conf()
+    spark = SparkSession.builder \
+        .config(conf=conf) \
+        .getOrCreate()
+    logger = Log4j(spark)
+
+    if len(sys.argv) != 2:
+        logger.error("Usage: 'main <filename>'")
+        sys.exit(-1)
+
+    logger.info("Starting Hello Spark!")
+    # logger.get_conf()
+
+    #Processing code
+
+    logger.info("Stopping Hello Spark!")
+    # spark.stop()
